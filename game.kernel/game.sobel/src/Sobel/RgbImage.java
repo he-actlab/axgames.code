483c4df331fa82473594db062afa0617d40f1dba
Jongse <jspark@gatech.edu>
2015-06-23 12:08:08 -0400

    game3 frontend almost done

--- game.kernel/game.sobel/src/Sobel/RgbImage.java
+++ game.kernel/game.sobel/src/Sobel/RgbImage.java
@@ -174,7 +174,7 @@ public class RgbImage {
 		imageSaver.save("-sobel.rgb", rgbImage.outputImage);
 		
 		Degrader degrader = new Degrader (rgbImage.width, rgbImage.height, rgbImage.outputImage, rgbImage.mode, 0.01);
-		double[] errors = {0.01, 0.03, 0.05, 0.07, 0.1, 0.2, 0.3, 0.4, 0.5};
+		double[] errors = {0.01, 0.03, 0.05, 0.07, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5};
 		degrader.degrade(errors, imageSaver);
 	}
 }
