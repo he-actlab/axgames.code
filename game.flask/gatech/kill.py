6806956d410145339676f4cb0ab17f6fc46c3ac2
Jongse <jspark@gatech.edu>
2015-06-26 15:35:29 -0400

    update

--- game.flask/gatech/kill.py
+++ game.flask/gatech/kill.py
@@ -2,5 +2,15 @@
 
 import os, sys
 
-os.system('killall -9 gunicorn')
+sysname = os.popen('uname').read().strip('\n')
+
+if sysname == 'Linux':
+	os.system('killall -9 gunicorn')
+
+elif sysname == 'Darwin':
+	os.system('pkill -f gunicorn > /dev/null 2> /dev/null ')
+	os.system('pkill -f redis-server > /dev/null 2> /dev/null ')
+
+else:
+	print 'Error! Unknown System'
 
--- game.flask/gatech/run.py
+++ game.flask/gatech/run.py
@@ -4,6 +4,7 @@ import os, sys
 
 sysname = os.popen('uname').read().strip('\n')
 
+# Deployment on Linux machine. addr: taftan.cc.gt.atl.ga.us
 if sysname == 'Linux':
 	os.system('sudo service redis-server restart')
 	os.system('sudo /etc/init.d/nginx restart')
@@ -13,18 +14,16 @@ if sysname == 'Linux':
 	print('gunicorn gatech:app --log-file=- 2>> ' + logname + ' >> ' + logname + ' &')
 	os.system('gunicorn gatech:app --log-file=- 2>> ' + logname + ' >> ' + logname + ' &')
 
+# Only for development. addr: localhost:5000 
 elif sysname == 'Darwin':
-	os.system('sudo service redis-server restart')
-	os.system('sudo /etc/init.d/nginx restart')
+	os.system('redis-server > /dev/null 2> /dev/null & ')
 
-	logname = os.popen('date +%m%d%Y_%H%M%S').read().strip('\n') + '.log'
+	logname = os.popen('date +%H%M%S_%m%d%Y').read().strip('\n') + '.log'
 
-	print('gunicorn gatech:app --log-file=- 2>> ' + logname + ' >> ' + logname + ' &')
-	os.system('gunicorn gatech:app --log-file=- 2>> ' + logname + ' >> ' + logname + ' &')
+	os.system('foreman start 2>> ' + logname + ' >> ' + logname + ' &')
 
 else:
-	os.system('Error! Unknown System')
-	sys.exit()
+	print 'Error! Unknown System'
 
 
 
