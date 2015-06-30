8cac1c8be5039385d91199c070dcb8eae5b116ec
Jongse <jspark@gatech.edu>
2015-06-30 13:09:19 -0400

    update

--- game.flask/gatech/gatech/game1_acceptable/query_acceptable.py
+++ game.flask/gatech/gatech/game1_acceptable/query_acceptable.py
@@ -5,82 +5,56 @@ import time
 from gatech import session
 from gatech.database import db_session
 from gatech.models import Image, DegradedImage, Play, PlaySession
+from gatech.conf import drawn_errors
 
 #
 # webpage update - fetch random input image files from database and throw them to the game page
 #
 def draw_acceptable_image_files():
-
+	os.system('echo draw_acceptable_image_files: start')
 	filePaths = []
-	os.system('echo draw_acceptable_image_files: 1')
-	# os.system('rm -rf gatech/static/img/' + session['sessionid'])
-	# os.system('echo rm -rf gatech/static/img/' + session['sessionid'])
 
-	os.system('echo draw_acceptable_image_files: 2')
 	drawnImageFilename = ''
 	for result in db_session.query(Image).order_by(Image.num_played_game1):
 		orgImageId = result.image_id
 		drawnImageFilename = result.imagename
-		# drawnImageFilename = result.filename
-		# drawnImageData = result.image_file
-		# drawnSblImageData = result.sbl_image_file
 		break
 
-	# os.system('echo draw_acceptable_image_files: 3')
-	# with open('gatech/static/img/' + session['sessionid'] + '/' + drawnImageFilename, 'wb') as f1:
-	# 	f1.write(drawnImageData)
-	# f1.close()
-
-	# os.system('echo draw_acceptable_image_files: 4')
-	# drawnSblImageFilename = drawnImageFilename.split('.png')[0] + '-sobel.png'
-	# with open('gatech/static/img/' + session['sessionid'] + '/' + drawnSblImageFilename, 'wb') as f2:
-	# 	f2.write(drawnSblImageData)
-	# f2.close()
-
-	os.system('echo draw_acceptable_image_files: 5')
 	filePaths.append('/orgimage/' + drawnImageFilename + '.png')
 	filePaths.append('/sobelimage/' + drawnImageFilename + '-sobel.png')
 
-	os.system('echo draw_acceptable_image_files: 6')
-	for degresult in db_session.query(DegradedImage).filter_by(org_image_id=orgImageId).\
-													 order_by(DegradedImage.num_played):
-		drawnImageFilename = degresult.imagename
-		# drawnImageFilename = degresult.filename
-		# drawnImageData = degresult.image_file
-		break
+	drawnImageFilename = -1
+	minPlayed = 100000
+	for error in drawn_errors:
+		for tmpResult in db_session.query(DegradedImage).filter_by(org_image_id=orgImageId).filter_by(error=float(error)/100):
+			if tmpResult.num_played < minPlayed:
+				minPlayed = tmpResult.num_played
+				drawnImageFilename = tmpResult.imagename
+			break
 
-	# with open('gatech/static/img/' + session['sessionid'] + '/' + drawnImageFilename, 'wb') as f2:
-	# 	f2.write(drawnImageData)
-	# f2.close()
-
-	os.system('echo draw_acceptable_image_files: 6-2 ' + str(drawnImageFilename))
 	filePaths.append('/degimage/' + drawnImageFilename + '.png')
-	os.system('echo draw_acceptable_image_files: 7 ' + str(filePaths))
+	os.system('echo draw_acceptable_image_files: end filepaths = ' + str(filePaths))
 
 	return filePaths
 
 def get_image_id(imagename):
+	os.system('echo get_image_id: start')
 	imagename = imagename.split('.png')[0]
-	os.system('echo get_image_id: ' + imagename)
+
 	image_id = ''
 	for result in db_session.query(Image).filter_by(imagename=imagename):
-		os.system('echo get_image_id: 1')
 		image_id = result.image_id
-		os.system('echo get_image_id: 2')
 		break
-	os.system('echo get_image_id: 3')
 	return image_id
 
 def get_degimage_id(imagename):
+	os.system('echo get_degimage_id: stat')
 	imagename = imagename.split('.png')[0]
-	os.system('echo get_degimage_id: ' + imagename)
+
 	image_id = ''
 	for result in db_session.query(DegradedImage).filter_by(imagename=imagename):
-		os.system('echo get_degimage_id: 1')
 		image_id = result.deg_image_id
-		os.system('echo get_degimage_id: 2')
 		break
-	os.system('echo get_degimage_id: 3')
 	return image_id
 
 def get_num_played(degImageId):
@@ -99,8 +73,8 @@ def get_num_decision(degImageId, decision):
 	return record
 
 def update_record(degImageId, decision):
+	os.system('echo update_record start')
 	for result in db_session.query(DegradedImage).filter_by(deg_image_id=degImageId):
-		os.system('echo update_record start')
 		if decision == "agree":
 			newNum = result.num_agree + 1
 			db_session.query(DegradedImage).filter(DegradedImage.deg_image_id == degImageId).\
@@ -109,10 +83,6 @@ def update_record(degImageId, decision):
 			newNum = result.num_disagree + 1
 			db_session.query(DegradedImage).filter(DegradedImage.deg_image_id == degImageId).\
 										update({"num_disagree": newNum})
-		# os.system('echo newNum = ' + str(newNum))
-		# for i in range(0, 10):
-		# 	time.sleep(1)
-		# os.system('echo sleep end')
 		db_session.commit()
 
 		# update num_played for degImage
