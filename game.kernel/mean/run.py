8c50da52b978b0c7db5b485aeb842a932c1a7a37
Jongse Park <jspark@gatech.edu>
2015-07-31 10:49:24 -0400

    mean updated

--- game.kernel/mean/run.py
+++ game.kernel/mean/run.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
 import os, sys
+from multiprocessing import Process
 
-basedir = os.environ['GAME_SOBEL']
+basedir = os.environ['GAME_MEAN']
 
-for filename in os.popen('ls images | grep png').readlines():
 
+
+def runMean(filename):
 	print
 	print 'Working on ' + filename
 	print 
@@ -18,18 +20,18 @@ for filename in os.popen('ls images | grep png').readlines():
 	
 	print "png2rgb.py done ... (png2rgb)"
 
-	os.system('mkdir ' + basedir + '/rgboutput/' + name)
-	os.system('mkdir ' + basedir + '/rgboutput/' + name + '/nrmse')
+	os.system('rm -rf ' + basedir + '/rgboutput/' + name + ' ; mkdir ' + basedir + '/rgboutput/' + name)
+	os.system('rm -rf ' + basedir + '/rgboutput/' + name + '/nrmse ; mkdir ' + basedir + '/rgboutput/' + name + '/nrmse')
 	#os.system('mkdir ' + basedir + '/rgboutput/' + name + '/psnr')
 	
-	os.system('java -cp ' + basedir + '/sobel.jar Sobel.RgbImage ' + basedir + '/images/' + rgbfilename + ' ' + basedir + '/rgboutput/' + name + ' nrmse')
-	print "sobel done ... (nrmse)"
+	os.system('java -cp ' + basedir + '/mean.jar Mean.RgbImage ' + basedir + '/images/' + rgbfilename + ' ' + basedir + '/rgboutput/' + name + ' nrmse')
+	print "mean done ... (nrmse)"
 	
-	#os.system('java -cp ' + basedir + '/sobel.jar Sobel.RgbImage ' + basedir + '/images/' + rgbfilename + ' ' + basedir + '/rgboutput/' + name + ' psnr')
-	#print "Sobel done ... (psnr)"
+	#os.system('java -cp ' + basedir + '/mean.jar Mean.RgbImage ' + basedir + '/images/' + rgbfilename + ' ' + basedir + '/rgboutput/' + name + ' psnr')
+	#print "Mean done ... (psnr)"
 	
-	os.system('mkdir ' + basedir + '/pngoutput/' + name)
-	os.system('mkdir ' + basedir + '/pngoutput/' + name + '/nrmse')
+	os.system('rm -rf ' + basedir + '/pngoutput/' + name + ' ; mkdir ' + basedir + '/pngoutput/' + name)
+	os.system('rm -rf ' + basedir + '/pngoutput/' + name + '/nrmse ; mkdir ' + basedir + '/pngoutput/' + name + '/nrmse')
 	#os.system('mkdir ' + basedir + '/pngoutput/' + name + '/psnr')
 	
 	os.system(basedir + '/png2rgbAll.py rgb2png ' + basedir + '/rgboutput/' + name + '/nrmse ' + basedir + '/pngoutput/' + name + '/nrmse')
@@ -40,3 +42,21 @@ for filename in os.popen('ls images | grep png').readlines():
 	
 	os.system('rm -rf ' + basedir + '/rgboutput/' + name)
 	os.system('rm ' + basedir + '/images/' + rgbfilename)
+
+def main():
+	newfiles = []
+	files = os.popen('ls images | grep png').readlines()
+	PARALLEL_WIDTH = 3
+	for f in files:
+		print f.strip('\n')
+		newfiles.append(f.strip('\n'))
+
+	for chunk in range(0, (len(newfiles) / PARALLEL_WIDTH) + 1):
+		pool = [Process(target=runMean, args=(str(newfiles[i + chunk * PARALLEL_WIDTH]),)) for i in range(min(PARALLEL_WIDTH * (chunk+1), len(newfiles)) - PARALLEL_WIDTH * chunk)]
+		for i in range(min(PARALLEL_WIDTH * (chunk + 1), len(newfiles)) - PARALLEL_WIDTH * chunk):
+			pool[i].start()
+		for p in pool:
+			p.join()
+
+if __name__ == '__main__':	
+	main()
