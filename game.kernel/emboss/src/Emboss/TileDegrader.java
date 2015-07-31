680a2bcb5560e0b9b699a6d595d38dc8cbb7c51b
Jongse <jspark@gatech.edu>
2015-07-31 11:22:24 -0400

    degrader updated

---
+++ game.kernel/emboss/src/Emboss/TileDegrader.java
@@ -0,0 +1,178 @@
+package Sobel;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Random;
+
+public class TileDegrader {
+	
+	public int width;
+	public int height;
+	public int[][][] image;
+	public String mode;
+	public double errorRange;
+	public String path;
+	
+	TileDegrader(int width, int height, int[][][] orgImage, String mode, double errorRange, String path) {
+		System.out.println("width: " + width);
+		this.width = width;
+		this.height = height;
+		initImage(orgImage);
+		this.mode = mode;
+		this.errorRange = errorRange;
+		this.path = path;
+	}
+	
+	public void initImage(int[][][] orgImage) {
+		image = new int[height][][];
+		for (int i=0; i<height; i++){
+			image[i] = new int[width][];
+			for(int j=0; j<width; j++){
+				image[i][j] = new int[3];
+			}
+		}
+		for (int i=0; i<height; i++) {
+			for(int j=0; j<width; j++) {
+				for (int k=0; k<3; k++) {
+					image[i][j][k] = orgImage[i][j][k];
+				}
+			}
+		}
+	}
+	
+	public void degrade (double[] errors, ImageSaver imageSaver) throws IOException {
+		int[][][] newImage;
+		newImage = new int[height][][];
+		for (int i=0; i<height; i++){
+			newImage[i] = new int[width][];
+			for(int j=0; j<width; j++){
+				newImage[i][j] = new int[3];
+			}
+		}
+		
+		Random r = new Random();			
+		for (int i=0; i<height; i++) {
+			for (int j=0; j<width; j++) {
+				for (int k=0; k<3; k++) {
+					newImage[i][j][k] = image[i][j][k];
+				}
+			}
+		}
+		
+		boolean find = false;
+		int h, w, diff, val;
+		int errIdx = 0;
+		int numFound = 0;
+		long sum = 0;
+		HashMap<String, Integer> diffMap = new HashMap<String, Integer>();
+		int orgValue;
+		
+		int count = 0; //debug
+		
+		int max = -1;
+		int min = 256;
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				if (image[i][j][0] > max)
+					max = image[i][j][0];
+				if (image[i][j][0] < min)
+					min = image[i][j][0];
+			}
+		}
+		
+		if (mode.equalsIgnoreCase("nrmse")) {
+			double nrmse;
+			do {
+				count++;
+				
+				h = (int)(r.nextDouble() * height);
+				w = (int)(r.nextDouble() * width);
+				
+				orgValue = newImage[h][w][0];
+				
+				if (w == width-1) 
+					newImage[h][w][0] = newImage[h][w][1] = newImage[h][w][2] = newImage[h][w-1][0];
+				else if (w == 0) 
+					newImage[h][w][0] = newImage[h][w][1] = newImage[h][w][2] = newImage[h][w+1][0];
+				else if (h == height-1) 
+					newImage[h][w][0] = newImage[h][w][1] = newImage[h][w][2] = newImage[h-1][w][0];
+				else if (h == 0) 
+					newImage[h][w][0] = newImage[h][w][1] = newImage[h][w][2] = newImage[h+1][w][0];
+				else {
+					double rd = r.nextDouble();
+					if (rd < 0.25)
+						newImage[h][w][0] = newImage[h][w][1] = newImage[h][w][2] = newImage[h][w-1][0];
+					else if (rd >= 0.25 && rd < 0.5)
+						newImage[h][w][0] = newImage[h][w][1] = newImage[h][w][2] = newImage[h][w+1][0];
+					else if (rd >= 0.5 && rd < 0.75)
+						newImage[h][w][0] = newImage[h][w][1] = newImage[h][w][2] = newImage[h-1][w][0];
+					else
+						newImage[h][w][0] = newImage[h][w][1] = newImage[h][w][2] = newImage[h+1][w][0];
+				}
+				
+				diff = Math.abs(newImage[h][w][0] - image[h][w][0]);
+				String key = String.valueOf(h) + 'x' + String.valueOf(w); 
+				if (diffMap.containsKey(key)) {
+					val = diffMap.get(key);
+					if (val < diff || r.nextDouble() > 0.05) {
+						sum -= val * val;
+						diffMap.put(key, diff);
+						sum += diff * diff;
+					} else {
+						newImage[h][w][0] = newImage[h][w][1] = newImage[h][w][2] = orgValue;
+					}
+				} else {
+					diffMap.put(key, diff);
+					sum += diff * diff;
+				}
+				nrmse = Math.sqrt(sum / (height * width)) / (max - min);
+				
+				if (nrmse > errors[errIdx] && nrmse < errors[errIdx] + errorRange) {
+					imageSaver.save("_" + errors[errIdx] + ".rgb", newImage);
+					System.out.println("File[" + path + "]    NRMSE: " + nrmse + "\tCount: " + count);
+					errIdx++;
+					numFound++;
+					long sum2 = 0;
+					for (int i=0; i < height; i++){
+						for (int j=0; j < width; j++){
+							diff = Math.abs(newImage[i][j][0] - image[i][j][0]);
+							sum2 += diff * diff;
+						}
+					}
+//					System.out.println("sum2: " + sum2);
+					System.out.println("Real NRMSE: " + Math.sqrt(sum2 / (height * width)) / (max - min));
+					if (numFound == errors.length)
+						find = true;
+				}
+			} while (!find);	
+		} 
+		else if (mode.equalsIgnoreCase("psnr")) {
+			double mse, psnr;
+			do {
+				count++;
+				
+				h = (int)(r.nextDouble() * height);
+				w = (int)(r.nextDouble() * width);
+				
+				newImage[h][w][0] = newImage[h][w][1] = newImage[h][w][2] = r.nextInt(255);
+				
+				diff = Math.abs(newImage[h][w][0] - image[h][w][0]);
+				sum += diff * diff;
+				mse = sum / (height * width);
+				psnr = 10 * Math.log10(255.0 * 255.0 / mse); 
+				
+				if (psnr > errors[errIdx] && psnr < errors[errIdx] + errorRange) {
+					imageSaver.save("_" + errors[errIdx] + ".rgb", newImage);
+					System.out.println("NRMSE: " + psnr + "\tCount: " + count);
+					errIdx++;
+					numFound++;
+					if (numFound == errors.length)
+						find = true;
+				}
+			} while (!find);	
+		} else {
+			System.out.println("Error: Unknown mode!");
+			System.exit(0);
+		}
+	}
+}
--- game.kernel/mean/src/Mean/RgbImage.java
+++ game.kernel/mean/src/Mean/RgbImage.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Vector;
 
+import Mean.TileDegrader;
+
 public class RgbImage {
 	public int width;
 	public int height;
@@ -169,7 +171,8 @@ public class RgbImage {
 		ImageSaver imageSaver = new ImageSaver (rgbImage.width, rgbImage.height, rgbImage.mode, rgbImage.inputFilePath, rgbImage.outDirPath);
 		imageSaver.save("-mean.rgb", rgbImage.outputImage);
 		
-		Degrader degrader = new Degrader (rgbImage.width, rgbImage.height, rgbImage.outputImage, rgbImage.mode, 0.01);
+//		Degrader degrader = new Degrader (rgbImage.width, rgbImage.height, rgbImage.outputImage, rgbImage.mode, 0.01);
+		TileDegrader degrader = new TileDegrader (rgbImage.width, rgbImage.height, rgbImage.outputImage, rgbImage.mode, 0.01, rgbImage.inputFilePath);
 		double[] errors = {0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 
 						   0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2,
 						   0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.3,
